2c725c93d6980be0ef7286252ad8186ce8859264
webra <li_zhm@qq.com>
2021-02-05 12:34:17 +0800

    Inform.vue 将时间参数原本错误的data 改为date 增加表单验证 表单验证重置按钮功能 提交处理修改

--- src/views/system/site/Inform.vue
+++ src/views/system/site/Inform.vue
@@ -4,7 +4,7 @@
       <el-button type="primary" plain icon="el-icon-plus" size="small" @click="openDialogAddInform">新 增</el-button>
       <el-button type="danger" plain icon="el-icon-delete" size="small" @click="deleteInforms">删 除</el-button>
       <el-input v-model="searchInformOption.title" size="small" style="margin-left: 10px;width: 200px;" placeholder="输入公告标题部分字段" @change="searchInform"></el-input>
-      <el-date-picker v-model="searchInformOption.data" type="daterange" start-placeholder="开始日期" end-placeholder="结束日期" size="small" :default-time="['00:00:00', '23:59:59']" style="margin-left: 10px" @change="searchInform"></el-date-picker>
+      <el-date-picker v-model="searchInformOption.date" type="daterange" start-placeholder="开始日期" end-placeholder="结束日期" size="small" :default-time="['00:00:00', '23:59:59']" style="margin-left: 10px" @change="searchInform"></el-date-picker>
       <el-select size="small" v-model="searchInformOption.mark" placeholder="公告重要程度" style="margin-left: 10px;" clearable @change="searchInform">
         <el-option v-for="item in markSelect" :key="item.value" :label="item.label" :value="item.value"></el-option>
       </el-select>
@@ -16,7 +16,7 @@
         <el-table-column type="selection" width="55"></el-table-column>
         <el-table-column prop="id" label="序号" width="55"></el-table-column>
         <el-table-column prop="title" label="公告标题"></el-table-column>
-        <el-table-column prop="data" label="时间"></el-table-column>
+        <el-table-column prop="date" label="时间"></el-table-column>
         <el-table-column prop="mark" label="重要标识"></el-table-column>
         <el-table-column label="操作">
           <template slot-scope="scope">
@@ -33,47 +33,36 @@
     </el-row>
 
     <el-dialog :title="dialogAddInformTitle+'公告'" :visible.sync="dialogAddInform">
-      <el-form :model="inform">
+      <el-form :model="inform" :rules="rules" ref="inform">
         <el-row>
-          <el-form-item label="公告标题" :label-width="formLabelWidth" required>
+          <el-form-item label="公告标题" :label-width="formLabelWidth" prop="title">
             <el-input v-model="inform.title" autocomplete="off"></el-input>
           </el-form-item>
         </el-row>
         <el-row>
           <el-col :sm="12" :xs="24">
-            <el-form-item label="发布日期" :label-width="formLabelWidth" required>
+            <el-form-item label="发布日期" :label-width="formLabelWidth" prop="date">
               <el-date-picker type="date" placeholder="选择日期" v-model="inform.date" style="width: 100%;"></el-date-picker>
             </el-form-item>
           </el-col>
           <el-col :sm="12" :xs="24">
-            <el-form-item label="重要程度" :label-width="formLabelWidth" required>
+            <el-form-item label="重要程度" :label-width="formLabelWidth" prop="mark">
               <el-select v-model="inform.mark" placeholder="请选择">
-                <el-option
-                  v-for="item in markSelect"
-                  :key="item.value"
-                  :label="item.label"
-                  :value="item.value">
-                </el-option>
+                <el-option v-for="item in markSelect" :key="item.value" :label="item.label" :value="item.value"></el-option>
               </el-select>
             </el-form-item>
           </el-col>
         </el-row>
         <el-row>
-          <el-form-item label="通知正文" :label-width="formLabelWidth" required>
-            <el-input
-              type="textarea"
-              placeholder="请输入通知内容"
-              v-model="inform.text"
-              show-word-limit
-              rows="6"
-            ></el-input>
+          <el-form-item label="通知正文" :label-width="formLabelWidth" prop="text">
+            <el-input type="textarea" placeholder="请输入通知内容" v-model="inform.text" show-word-limit rows="6"></el-input>
           </el-form-item>
         </el-row>
       </el-form>
       <div slot="footer" class="dialog-footer">
-        <el-button @click="dialogAddInform = false">取 消</el-button>
-        <el-button @click="clearInform" type="warning" plain style="float:left;">清 空</el-button>
-        <el-button type="primary" @click="saveInform">确 定</el-button>
+        <el-button @click="clearInform;dialogAddInform = false">取 消</el-button>
+        <el-button @click="clearInform" type="warning" plain style="float:left;">清 空 重 置</el-button>
+        <el-button type="primary" @click="saveInform('inform')">确 定</el-button>
       </div>
     </el-dialog>
   </div>
@@ -88,36 +77,50 @@ export default {
     return {
       searchInformOption: {
         title: '',
-        data: '',
+        date: '',
         mark: ''
+      },
+      rules: {
+        title: [
+          { required: true, message: '请输入公告标题', trigger: 'blur' }
+        ],
+        date: [
+          { required: true, message: '请选择日期', trigger: 'blur' }
+        ],
+        mark: [
+          { required: true, message: '请选择公告类型', trigger: 'change' }
+        ],
+        text: [
+          { required: true, message: '请输入公告正文内容', trigger: 'blur' }
+        ]
       },
       tableData: [
         {
           id: '1',
           title: '我是公告标题1',
-          data: '时间1',
+          date: '时间1',
           mark: '1'
         }, {
           id: '2',
           title: '我是公告标题2',
-          data: '时间2',
+          date: '时间2',
           mark: '2'
         }, {
           id: '3',
           title: '我是公告标题3',
-          data: '时间3',
+          date: '时间3',
           mark: '3'
         }, {
           id: '4',
           title: '我是公告标题4',
-          data: '时间4',
+          date: '时间4',
           mark: '4'
         }
       ],
       inform: {
         title: '',
-        data: '',
-        mark: '',
+        date: '',
+        mark: 'success',
         text: ''
       },
       dialogAddInform: false,
@@ -150,7 +153,6 @@ export default {
      */
     openDialogAddInform () {
       this.clearInform()
-      this.inform.mark = 'success'
       this.dialogAddInformTitle = '新增'
       this.dialogAddInform = true
     },
@@ -158,9 +160,11 @@ export default {
      * 清空表单数据
      */
     clearInform () {
-      for (const key in this.inform) {
-        this.inform[key] = ''
-      }
+      this.$refs.inform.resetFields()
+      this.inform.title = ''
+      this.inform.date = ''
+      this.inform.mark = 'success'
+      this.inform.text = ''
     },
     /**
      * 将获取的行数据赋值到对话框进行更新
@@ -174,14 +178,20 @@ export default {
     /**
      * 保存对话框中的数据到后台数据库（更新和新增同一接口）
      */
-    saveInform () {
-      this.$confirm('是否确认' + this.dialogAddInformTitle + '名称为"' + this.inform.title + '"的公告通知?', '提示', {
-        confirmButtonText: '确定',
-        cancelButtonText: '取消',
-        type: 'warning'
-      }).then(() => {
-        this.$message.success(this.dialogAddInformTitle + '成功!')
-        this.dialogAddInform = false
+    saveInform (inform) {
+      this.$refs[inform].validate((valid) => {
+        if (valid) {
+          this.$confirm('是否确认' + this.dialogAddInformTitle + '名称为"' + this.inform.title + '"的公告通知?', '提示', {
+            confirmButtonText: '确定',
+            cancelButtonText: '取消',
+            type: 'warning'
+          }).then(() => {
+            this.$message.success(this.dialogAddInformTitle + '成功!')
+            this.dialogAddInform = false
+          })
+        } else {
+          return false
+        }
       })
     },
     /**
