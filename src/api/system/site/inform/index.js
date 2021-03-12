83d36c75be23e768553094acfe6f1e6c746c3c27
webra <li_zhm@qq.com>
2021-03-12 18:39:02 +0800

    丰富公告接口

--- src/api/system/site/inform/index.js
+++ src/api/system/site/inform/index.js
@@ -1,9 +1,45 @@
 import request from '@/utils/request/index'
 
-export function getInform (params) {
+/**
+ * 获取公告列表
+ * @param {*} params 各种参数
+ * @returns informList
+ */
+export function getInformList (params) {
   return request({
-    url: '/system/site/inform',
+    url: '/system/site/inform/get/list',
     method: 'get',
+    params
+  })
+}
+
+/**
+ * 获取单条数据的详细信息
+ * @param {*} params  id
+ * @returns inform
+ */
+export function getInform (id) {
+  return request({
+    url: '/system/site/inform/get',
+    method: 'get',
+    params: {
+      id: id
+    }
+  })
+}
+
+/**
+ * 新建/修改 单条 数据
+ * @param {*} params  params
+ * @returns inform
+ */
+export function saveInform (params) {
+  return request({
+    url: '/system/site/inform/save',
+    method: 'post',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
     params
   })
 }
