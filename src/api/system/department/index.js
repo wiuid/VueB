b8c7da52eb8d5dcc1740a2d9012ac77b8dc3b3d8
webra <li_zhm@qq.com>
2021-03-19 17:04:04 +0800

    部门接口

--- src/api/system/department/index.js
+++ src/api/system/department/index.js
@@ -6,3 +6,48 @@ export function getDepartmentList () {
     method: 'get'
   })
 }
+
+export function getDepartmentById (id) {
+  return request({
+    url: '/system/department/select/id',
+    method: 'get',
+    params: {
+      id: id
+    }
+  })
+}
+
+export function getDepartmentSelect () {
+  return request({
+    url: '/system/department/select/tree',
+    method: 'get'
+  })
+}
+
+export function saveDepartment (params) {
+  return request({
+    url: '/system/department/save',
+    method: 'post',
+    data: params
+  })
+}
+
+export function updateState (id) {
+  return request({
+    url: '/system/department/update/state',
+    method: 'post',
+    data: {
+      id: id
+    }
+  })
+}
+
+export function delectDepartment (id) {
+  return request({
+    url: '/system/department/delete',
+    method: 'delete',
+    params: {
+      id: id
+    }
+  })
+}
