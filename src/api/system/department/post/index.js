687a8162378cd7fb3b70fa54352abd8916e6335d
webra <li_zhm@qq.com>
2021-03-19 17:03:52 +0800

    岗位接口

---
+++ src/api/system/department/post/index.js
@@ -0,0 +1,57 @@
+import request from '@/utils/request/index'
+
+export function getPostList (params) {
+  return request({
+    url: '/system/department/post',
+    method: 'get',
+    params
+  })
+}
+
+export function getPostById (id) {
+  return request({
+    url: '/system/department/post/select',
+    method: 'get',
+    params: {
+      id: id
+    }
+  })
+}
+
+export function savePost (params) {
+  return request({
+    url: '/system/department/post/save',
+    method: 'post',
+    data: params
+  })
+}
+
+export function updatePostState (id) {
+  return request({
+    url: '/system/department/post/state',
+    method: 'post',
+    data: {
+      id: id
+    }
+  })
+}
+
+export function deletePost (id) {
+  return request({
+    url: '/system/department/post/delete',
+    method: 'delete',
+    params: {
+      id: id
+    }
+  })
+}
+
+export function deletePosts (ids) {
+  return request({
+    url: '/system/department/post/deletes',
+    method: 'delete',
+    params: {
+      ids: ids
+    }
+  })
+}
