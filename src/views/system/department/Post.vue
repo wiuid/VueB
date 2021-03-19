250b06d3bf85ff9f897ccb4d745816e2be66c22a
webra <li_zhm@qq.com>
2021-03-19 17:05:21 +0800

    岗位页面 接口对接

--- src/views/system/department/Post.vue
+++ src/views/system/department/Post.vue
@@ -1,10 +1,12 @@
 <template>
   <div>
     <el-row style="margin-bottom: 10px">
-      <el-input v-model="searchPostOption.title"  style="margin-right: 10px;width: 200px;" placeholder="输入岗位名称部分字段" @change="searchPost" clearable></el-input>
-      <el-select  v-model="searchPostOption.state" placeholder="状态分类" style="margin-right: 10px;" clearable @change="searchPost">
+      <el-input v-model="params.title"  style="margin-right: 10px;width: 200px;" placeholder="输入岗位名称部分字段" clearable></el-input>
+      <el-select  v-model="params.state" placeholder="请选择状态" style="margin-right: 10px;" clearable>
         <el-option v-for="item in stateSelect" :key="item.value" :label="item.label" :value="item.value"></el-option>
       </el-select>
+      <el-button type="primary" plain icon="el-icon-refresh" @click="clearSearchPost">重置</el-button>
+      <el-button type="primary" plain icon="el-icon-search" @click="searchPost">搜索</el-button>
     </el-row>
     <el-row>
       <el-button type="primary" plain icon="el-icon-plus"  @click="openDialogAddPost">新 增</el-button>
@@ -12,14 +14,28 @@
       <el-button type="success" plain icon="el-icon-download"  @click="deletePosts">导 出</el-button>
     </el-row>
     <el-row>
-      <el-table v-loading="loading" :data="tableData" stripe style="width: 100%" ref="informTable"  @selection-change="handleSelectionChange">
-        <el-table-column type="selection" width="55"></el-table-column>
-        <el-table-column prop="id" label="序号" width="55"></el-table-column>
-        <el-table-column prop="title" label="岗位名称"></el-table-column>
-        <el-table-column prop="serial" label="排序"></el-table-column>
-        <el-table-column prop="state" label="状态"></el-table-column>
-        <el-table-column prop="date" label="时间"></el-table-column>
-        <el-table-column label="操作">
+      <el-table v-loading="loading" :data="tableData" stripe style="width: 100%" ref="informTable"  @selection-change="handleSelectionChange" :header-cell-style="{'text-align':'center'}">
+        <el-table-column type="selection" width="55" align="center"></el-table-column>
+        <el-table-column type="index" label="序号" width="55" align="center"></el-table-column>
+        <el-table-column prop="id" v-if="false" align="center"></el-table-column>
+        <el-table-column prop="title" label="岗位名称" align="center"></el-table-column>
+        <el-table-column prop="serial" label="排序" align="center"></el-table-column>
+        <el-table-column prop="state" label="状态" align="center">
+          <template slot-scope="scope">
+            <el-switch
+            v-model="scope.row.state"
+            :active-value="0"
+            :inactive-value="1"
+            @change="editSwitch(scope.row.id)">
+            </el-switch>
+          </template>
+        </el-table-column>
+        <el-table-column prop="createDate" label="时间" align="center">
+          <template slot-scope="scope">
+            {{scope.row.createDate | formatData()}}
+          </template>
+        </el-table-column>
+        <el-table-column label="操作" align="center">
           <template slot-scope="scope">
             <el-link :underline="false" type="primary" style="margin-right: 10px;font-size: 10px" @click="editPost(scope.row.id)"><i class="el-icon-edit"></i>修改</el-link>
             <el-link :underline="false" type="primary" style="margin-right: 10px;font-size: 10px" @click="deletePost(scope.row)"><i class="el-icon-delete"></i>删除</el-link>
@@ -27,8 +43,11 @@
         </el-table-column>
       </el-table>
     </el-row>
+
     <el-row style="text-align: center">
-      <pagination :total="20"></pagination>
+      <pagination :total="total"
+      :pager="params.page"
+      :current="pageJump"></pagination>
     </el-row>
 
     <el-dialog :title="dialogAddPostTitle+'岗位'" :visible.sync="dialogAddPost" :before-close="closeDialog">
@@ -77,30 +96,17 @@
 
 <script>
 import pagination from '@/components/Pagination'
+import { getPostList, getPostById, savePost, deletePost, deletePosts, updatePostState } from '@/api/system/department/post'
 export default {
   name: 'Post',
   components: { pagination },
   data () {
     return {
-      tableData: [
-        {
-          id: '1',
-          title: '董事长',
-          serial: '1',
-          state: '正常',
-          date: '2021/2/19'
-        },
-        {
-          id: '2',
-          title: '总经理',
-          serial: '2',
-          state: '正常',
-          date: '2021/2/19'
-        }
-      ],
+      tableData: [],
       post: {
+        id: 0,
         title: '',
-        serial: '',
+        serial: 0,
         state: 0,
         remark: ''
       },
@@ -114,10 +120,12 @@ export default {
           { required: true, message: '请输入岗位名称', trigger: 'blur' }
         ]
       },
-      searchPostOption: {
+      params: {
         title: '',
-        state: ''
+        state: '',
+        page: 1
       },
+      total: 0,
       stateSelect: [
         {
           label: '正常',
@@ -128,6 +136,21 @@ export default {
           value: '1'
         }
       ]
+    }
+  },
+  mounted () {
+    this.searchPost()
+  },
+  filters: {
+    formatData (time) {
+      var data = new Date(time)
+      var y = data.getFullYear()
+      var M = data.getMonth() + 1
+      var d = data.getDate()
+
+      var h = data.getHours()
+      var m = data.getMinutes()
+      return y + '-' + M + '-' + d + ' ' + h + ':' + m
     }
   },
   methods: {
@@ -143,6 +166,18 @@ export default {
      */
     editPost (id) {
       this.dialogAddPostTitle = '修改'
+      const res = new Promise((resolve, reject) => {
+        getPostById(id).then((result) => { resolve(result) }).catch((err) => { reject(err) })
+      })
+      res.then((result) => {
+        if (result.status === 200) {
+          this.post.id = result.data.post.id
+          this.post.title = result.data.post.title
+          this.post.serial = result.data.post.serial
+          this.post.state = result.data.post.state
+          this.post.remark = result.data.post.remark
+        }
+      })
       this.dialogAddPost = true
       /**
        * 这里通过传入的id参数从后台获取数据传入到post表单
@@ -152,14 +187,19 @@ export default {
      * 离开对话框并清除表单规则
      */
     closeDialog () {
-      this.clearPost()
       this.dialogAddPost = false
+      this.clearPost()
     },
     /**
      * 清除对话框表单规则
      */
     clearPost () {
       this.$refs.post.resetFields()
+      this.post.id = 0
+      this.post.title = ''
+      this.post.serial = 0
+      this.post.state = 0
+      this.post.remark = ''
     },
     /**
      * 保存 新增/修改 的岗位信息
@@ -172,11 +212,30 @@ export default {
             cancelButtonText: '取消',
             type: 'warning'
           }).then(() => {
-            this.$message.success(this.dialogAddPostTitle + '岗位成功!')
-            this.dialogAddPost = false
+            const res = new Promise((resolve, reject) => {
+              savePost(this.post).then((result) => { resolve(result) }).catch((err) => reject(err))
+            })
+            res.then((result) => {
+              if (result.status === 200) {
+                this.$message.success(result.msg)
+                this.searchPost()
+                this.closeDialog()
+              }
+            })
           })
         } else {
           return false
+        }
+      })
+    },
+    editSwitch (id) {
+      const res = new Promise((resolve, reject) => {
+        updatePostState(id).then((result) => { resolve(result) }).catch((err) => { reject(err) })
+      })
+      res.then((result) => {
+        if (result.status === 200) {
+          this.$message.success(result.msg)
+          this.searchPost()
         }
       })
     },
@@ -185,8 +244,24 @@ export default {
      */
     searchPost () {
       this.loading = true
-      this.$message.success('触发搜索API')
-      this.loading = false
+      const res = new Promise((resolve, reject) => {
+        getPostList(this.params).then((result) => { resolve(result) }).catch((err) => { reject(err) })
+      })
+      res.then((result) => {
+        this.tableData = result.data.postList
+        this.total = result.data.total
+        this.loading = false
+      })
+    },
+    pageJump (page) {
+      this.params.page = page
+      this.searchPost()
+    },
+    clearSearchPost () {
+      this.params.title = ''
+      this.params.state = null
+      this.params.page = 1
+      this.searchPost()
     },
     /**
      * 删除单个岗位
@@ -197,8 +272,16 @@ export default {
         cancelButtonText: '取消',
         type: 'warning'
       }).then(() => {
-        this.$message.success('删除成功!')
-        this.dialogAddInform = false
+        const res = new Promise((resolve, reject) => {
+          deletePost(row.id).then((result) => { resolve(result) }).catch((err) => { reject(err) })
+        })
+        res.then((result) => {
+          if (result.status === 200) {
+            this.closeDialog()
+            this.$message.success(result.msg)
+            this.searchPost()
+          }
+        })
       })
     },
     /**
@@ -207,16 +290,27 @@ export default {
     deletePosts () {
       const postList = this.multipleSelection
       const idList = []
+      const titleList = []
       if (postList.length !== 0) {
         for (const index in postList) {
           idList.push(postList[index].id)
+          titleList.push(postList[index].title)
         }
-        this.$confirm('是否确认删除序号为"' + idList.toString() + '"的日志信息?', '提示', {
+        this.$confirm('是否确认删除名称为"' + titleList.toString() + '"的岗位?', '提示', {
           confirmButtonText: '确定',
           cancelButtonText: '取消',
           type: 'warning'
         }).then(() => {
-          this.$message.success('删除成功!')
+          const res = new Promise((resolve, reject) => {
+            deletePosts(idList.toString()).then((result) => { resolve(result) }).catch((err) => { reject(err) })
+          })
+          res.then((result) => {
+            if (result.status === 200) {
+              this.closeDialog()
+              this.$message.success(result.msg)
+              this.searchPost()
+            }
+          })
         })
       } else {
         this.$message.warning('你应该至少选中一个！')
