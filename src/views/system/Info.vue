780c1f845cd954f37dbb738e806124bbf8f9a75f
webra <li_zhm@qq.com>
2021-03-22 20:53:59 +0800

    接口对接

--- src/views/system/Info.vue
+++ src/views/system/Info.vue
@@ -20,32 +20,32 @@
             <el-divider class="costHr"></el-divider>
             <el-row>
               <el-col span="12"><i class="el-icon-user"></i>用户账号</el-col>
-              <el-col span="12"><span>admin</span></el-col>
+              <el-col span="12">{{userInfo.username}}</el-col>
             </el-row>
             <el-divider class="costHr"></el-divider>
             <el-row>
               <el-col span="12"><i class="el-icon-mobile-phone"></i>手机号码</el-col>
-              <el-col span="12"><span>18231200000</span></el-col>
+              <el-col span="12">{{userInfo.phone | formatNull()}}</el-col>
             </el-row>
             <el-divider class="costHr"></el-divider>
             <el-row>
               <el-col span="12"><i class="el-icon-message"></i>用户邮箱</el-col>
-              <el-col span="12"><span>li_zhm@qq.com</span></el-col>
+              <el-col span="12">{{userInfo.email | formatNull()}}</el-col>
             </el-row>
             <el-divider class="costHr"></el-divider>
             <el-row>
               <el-col span="12"><i class="el-icon-office-building"></i>所属部门</el-col>
-              <el-col span="12"><span>Webra科技 / 董事长</span></el-col>
+              <el-col span="12">{{userInfo.department | formatNull()}}</el-col>
             </el-row>
             <el-divider class="costHr"></el-divider>
             <el-row>
               <el-col span="12"><i class="el-icon-finished"></i>所属角色</el-col>
-              <el-col span="12"><span>超级管理员</span></el-col>
+              <el-col span="12">{{userInfo.role}}</el-col>
             </el-row>
             <el-divider class="costHr"></el-divider>
             <el-row>
               <el-col span="12"><i class="el-icon-date"></i>创建日期</el-col>
-              <el-col span="12"><span>2021/2/19</span></el-col>
+              <el-col span="12">{{userInfo.createDate}}</el-col>
             </el-row>
             <el-divider class="costHr"></el-divider>
           </div>
@@ -56,11 +56,11 @@
           <div slot="header" class="clearfix">
             <span>基本资料</span>
           </div>
-          <el-tabs v-model="activeName" @tab-click="handleClick">
+          <el-tabs v-model="activeName">
             <el-tab-pane label="基本资料" name="first">
               <el-form label-position="right" label-width="80px" :model="info" :rules="rulesInfo" ref="rulesInfo">
-                <el-form-item label="用户昵称" prop="name" required>
-                  <el-input v-model="info.name" autocomplete="off" style="width: 300px;"></el-input>
+                <el-form-item label="用户昵称" prop="nickname" required>
+                  <el-input v-model="info.nickname" autocomplete="off" style="width: 300px;"></el-input>
                 </el-form-item>
                 <el-form-item label="手机号码" prop="phone">
                   <el-input type="number" v-model="info.phone" autocomplete="off" style="width: 300px;"></el-input>
@@ -120,6 +120,7 @@
 </template>
 
 <script>
+import { getData, updateInfo, updatePassword } from '@/api/system/user/info'
 export default {
   name: 'Info',
   data () {
@@ -142,8 +143,8 @@ export default {
     const validateNewPassword = (rule, value, callback) => {
       if (value === '') {
         callback(new Error('请输入新密码'))
-      } else if (value.length < 8) {
-        callback(new Error('至少输入8位新密码'))
+      } else if (value.length < 6) {
+        callback(new Error('至少输入6位新密码'))
       } else {
         callback()
       }
@@ -159,16 +160,17 @@ export default {
     }
     return {
       userInfo: {
+        id: 0,
         portrait: '',
-        username: 'admin',
-        phone: '18231202665',
-        email: 'li_zhm@qq.com',
-        department: 'Webra科技 / 董事长',
-        auth: '超级管理员',
-        date: '2021/2/19'
+        username: '',
+        phone: '',
+        email: '',
+        department: '',
+        role: '',
+        createDate: ''
       },
       info: {
-        name: '',
+        nickname: '',
         phone: '',
         email: ''
       },
@@ -180,7 +182,7 @@ export default {
       labelPosition: 'right',
       activeName: 'first',
       rulesInfo: {
-        name: [
+        nickname: [
           { validator: validateName, trigger: 'blur' }
         ],
         phone: [
@@ -204,7 +206,37 @@ export default {
       dialogChangePortrait: false
     }
   },
+  filters: {
+    formatNull (param) {
+      if (param === null || param === '') {
+        return '暂无'
+      } else {
+        return param
+      }
+    }
+  },
+  mounted () {
+    this.getTableData()
+  },
   methods: {
+    getTableData () {
+      const res = new Promise((resolve, reject) => {
+        getData().then((result) => { resolve(result) })
+      })
+      res.then((result) => {
+        this.userInfo.id = result.data.user.id
+        this.userInfo.username = result.data.user.username
+        this.info.nickname = result.data.user.nickname
+        this.userInfo.phone = result.data.user.phone
+        this.info.phone = result.data.user.phone
+        this.userInfo.email = result.data.user.email
+        this.info.email = result.data.user.email
+        this.userInfo.phone = result.data.user.phone
+        this.userInfo.department = result.data.departmentTitle
+        this.userInfo.role = result.data.roleTitle
+        this.userInfo.createDate = result.data.user.createDate
+      })
+    },
     /**
      * 保存基本资料
      * @param rules
@@ -212,9 +244,17 @@ export default {
     saveInfo (rules) {
       this.$refs[rules].validate((valid) => {
         if (valid) {
-          this.$message.info('规则正确，正在提交数据')
+          const res = new Promise((resolve, reject) => {
+            this.info.id = this.userInfo.id
+            updateInfo(this.info).then((result) => { resolve(result) })
+          })
+          res.then((result) => {
+            if (result.status === 200) {
+              this.$message.success(result.msg)
+              this.getTableData()
+            }
+          })
         } else {
-          this.$message.info('某规则不符合要求，请重新修改')
           return false
         }
       })
@@ -226,19 +266,23 @@ export default {
     savePassword (rules) {
       this.$refs[rules].validate((valid) => {
         if (valid) {
-          this.$message.info('规则正确，正在提交数据')
+          const res = new Promise((resolve, reject) => {
+            this.passwordForm.id = this.userInfo.id
+            updatePassword(this.passwordForm).then((result) => { resolve(result) })
+          })
+          res.then((result) => {
+            if (result.status === 200) {
+              sessionStorage.removeItem('token')
+              localStorage.removeItem('token')
+              this.$message.success(result.msg)
+              this.$router.replace('/login')
+            }
+          })
         } else {
           this.$message.info('某规则不符合要求，请重新修改')
           return false
         }
       })
-    },
-    /**
-     * 清空表单校验规则
-     */
-    handleClick () {
-      this.$refs.rulesInfo.resetFields()
-      this.$refs.rulesPassword.resetFields()
     },
     /**
      * 保存头像
