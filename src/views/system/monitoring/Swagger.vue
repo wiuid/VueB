71a00eb60fa6289e74c38e25bebd0d2db3888ec5
webra <li_zhm@qq.com>
2021-04-03 13:59:03 +0800

    新增swagger窗口

---
+++ src/views/system/monitoring/Swagger.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <iframe src="http://192.168.0.110:8080/api/swagger-ui/index.html#/"></iframe>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Swagger',
+  data () {
+  },
+  methods: {
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
