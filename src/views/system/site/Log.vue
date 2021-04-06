205d36dbfa9a2cc991e842882332dd78e1e2c618
webra <li_zhm@qq.com>
2021-04-06 10:23:41 +0800

    init

--- src/views/system/site/Log.vue
+++ src/views/system/site/Log.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
     <el-row style="margin-bottom: 10px">
-      <el-input v-model="searchLogOption.title"  style="margin-right: 10px;width: 200px;" placeholder="输入日志标题部分字段" @change="searchLog"></el-input>
-      <el-date-picker v-model="searchLogOption.data" type="daterange" start-placeholder="开始日期" end-placeholder="结束日期"  :default-time="['00:00:00', '23:59:59']" style="margin-right: 10px" @change="searchLog"></el-date-picker>
-      <el-select  v-model="searchLogOption.mark" placeholder="日志分类" style="margin-right: 10px;" clearable @change="searchLog">
+      <el-input v-model="params.title"  style="margin-right: 10px;width: 200px;" placeholder="输入日志标题部分字段" @change="searchLog"></el-input>
+      <el-date-picker v-model="searchDate" type="daterange" start-placeholder="开始日期" end-placeholder="结束日期"  :default-time="['00:00:00', '23:59:59']" style="margin-right: 10px" @change="searchLog"></el-date-picker>
+      <!-- <el-select  v-model="searchLogOption.mark" placeholder="日志分类" style="margin-right: 10px;" clearable @change="searchLog">
         <el-option v-for="item in markSelect" :key="item.value" :label="item.label" :value="item.value"></el-option>
-      </el-select>
+      </el-select> -->
     </el-row>
     <el-row>
       <el-button type="danger" plain icon="el-icon-delete"  @click="deleteLogs">删 除</el-button>
@@ -13,22 +13,31 @@
       <el-button type="danger" icon="el-icon-brush"  @click="deleteLogs">清 空</el-button>
     </el-row>
     <el-row>
-      <el-table v-loading="loading" :data="tableData" stripe style="width: 100%" ref="logTable" size="mini"  @selection-change="handleSelectionChange">
-        <el-table-column type="selection" width="55"></el-table-column>
-        <el-table-column prop="id" label="序号" width="55"></el-table-column>
-        <el-table-column prop="title" label="日志标题"></el-table-column>
-        <el-table-column prop="date" label="时间"></el-table-column>
-        <el-table-column prop="text" label="详情"></el-table-column>
+      <el-table v-loading="loading" :data="tableData" stripe style="width: 100%" ref="logTable" size="mini"  @selection-change="handleSelectionChange" :header-cell-style="{'text-align':'center'}">
+        <el-table-column type="selection" width="55" align="center"></el-table-column>
+        <el-table-column label="序号" type="index" width="55" align="center"></el-table-column>
+        <el-table-column prop="id" v-if="false"></el-table-column>
+        <el-table-column prop="title" label="日志标题" width="150" align="center"></el-table-column>
+        <el-table-column prop="username" label="操作人员" width="100" align="center"></el-table-column>
+        <el-table-column prop="createDate" label="创建时间" width="200" align="center">
+          <template slot-scope="scope">
+            {{scope.row.createDate | formatData()}}
+          </template>
+        </el-table-column>
+        <el-table-column prop="text" label="详情" align="center"></el-table-column>
       </el-table>
     </el-row>
     <el-row style="text-align: center">
-      <pagination :total="20"></pagination>
+      <pagination :total="total"
+      :pager="params.page"
+      :current="pageJump"></pagination>
     </el-row>
   </div>
 </template>
 
 <script>
 import pagination from '@/components/Pagination'
+import { getData } from '@/api/system/site/log'
 export default {
   name: 'Log',
   components: { pagination },
@@ -36,50 +45,57 @@ export default {
     return {
       multipleSelection: [],
       loading: false,
-      tableData: [
-        {
-          id: '1',
-          title: '一',
-          date: '',
-          text: ''
-        },
-        {
-          id: '2',
-          title: '二',
-          date: '',
-          text: ''
-        }
-      ],
-      searchLogOption: {
+      tableData: [],
+      params: {
         title: '',
-        date: '',
-        mark: ''
+        createDateStart: '',
+        createDateEnd: '',
+        page: 1
       },
-      markSelect: [
-        {
-          label: '1',
-          value: '一级'
-        },
-        {
-          label: '2',
-          value: '二级'
-        },
-        {
-          label: '3',
-          value: '三级'
-        },
-        {
-          label: '4',
-          value: '四级'
-        },
-        {
-          label: '5',
-          value: '五级'
-        }
-      ]
+      total: 0,
+      searchDate: []
+    }
+  },
+  mounted () {
+    this.getTableData()
+  },
+  filters: {
+    formatData (time) {
+      var data = new Date(time)
+      var y = data.getFullYear()
+      var M = data.getMonth() + 1
+      var d = data.getDate()
+
+      var h = data.getHours()
+      var m = data.getMinutes()
+      return y + '-' + M + '-' + d + ' ' + h + ':' + m
     }
   },
   methods: {
+    getTableData () {
+      this.loading = true
+      // 时间处理
+      if (this.searchDate.length !== 0) {
+        this.params.createDateStart = this.searchDate[0]
+        this.params.createDateEnd = this.searchDate[1]
+      }
+      // 数据请求
+      const res = new Promise((resolve, reject) => {
+        getData(this.params).then((result) => { resolve(result) })
+      })
+      res.then((result) => {
+        if (result.status === 200) {
+          this.tableData = result.data.logList
+          this.total = result.data.total
+        }
+        this.loading = false
+      })
+    },
+    // 页码跳转
+    pageJump (page) {
+      this.params.page = page
+      this.getTableData()
+    },
     /**
      * 批量删除日志
      */
