f513fca25e82a475f65cbe11e28f6b2d564ff72f
webra <li_zhm@qq.com>
2021-04-07 12:27:32 +0800

    全屏/导航显示/源码地址跳转

--- src/components/NavigationBarHeader.vue
+++ src/components/NavigationBarHeader.vue
@@ -4,18 +4,40 @@
       <i v-if="close" class="el-icon-s-fold"></i>
       <i v-if="open" class="el-icon-s-unfold"></i>
     </el-menu-item>
-
+    <el-menu-item>
+        <el-breadcrumb separator="/" style="margin-top:20px">
+          <el-breadcrumb-item :to="{ path: '/system' }">首页</el-breadcrumb-item>
+          <el-breadcrumb-item v-if="nav !== null">
+            {{nav}}
+          </el-breadcrumb-item>
+        </el-breadcrumb>
+    </el-menu-item>
     <el-submenu style="float: right" index="2">
       <template slot="title">
-        <img :src="avatar" style="height:40px; wight:40px; border-radius: 10px"/>
+        <img  v-if="fullIf" :src="avatar" style="height:40px; wight:40px; border-radius: 10px"/>
       </template>
       <el-menu-item index="/system/info">个人中心</el-menu-item>
       <el-menu-item @click="logout">退出登录</el-menu-item>
     </el-submenu>
+    <el-menu-item style="float: right" @click="full">
+      <img v-if="fullIf" src="@/assets/icon/ic-enlarge.svg" title="全屏" height="20px">
+      <img v-if="!fullIf" src="@/assets/icon/ic-narrow.svg" title="缩小" height="20px">
+    </el-menu-item>
+    <el-menu-item style="float: right">
+      <a href="https://github.com/wiuid/webraAdminA" target="_blank">
+        <img src="@/assets/icon/ic-github.svg" title="后端地址" height="20px">
+      </a>
+    </el-menu-item>
+    <el-menu-item style="float: right">
+      <a href="https://github.com/wiuid/VueA" target="_blank">
+        <img src="@/assets/icon/ic-github.svg" title="前端地址" height="20px">
+      </a>
+    </el-menu-item>
   </el-menu>
 </template>
 
 <script>
+import screenfull from 'screenfull'
 import { logout } from '@/api/login'
 import { getAvatar } from '@/api/system/user/info'
 export default {
@@ -23,10 +45,16 @@ export default {
   props: ['close', 'open', 'off'],
   data () {
     return {
-      avatar: ''
+      avatar: '',
+      fullIf: true,
+      nav: null
     }
   },
-  mounted () {
+  watch: {
+    $route: 'getNavList'
+  },
+  created () {
+    this.getNavList()
     getAvatar().then((result) => {
       if (result.status === 200) {
         this.avatar = result.data.avatar
@@ -36,6 +64,14 @@ export default {
   methods: {
     handleSelect (key, keyPath) {
       // console.log(key, keyPath)
+    },
+    getNavList () {
+      var title = this.$route.meta.title
+      if (title === '首页') {
+        this.nav = null
+      } else {
+        this.nav = title
+      }
     },
     logout () {
       const token = sessionStorage.getItem('token')
@@ -64,6 +100,10 @@ export default {
       } else {
         this.$message.error('您当前未登录')
       }
+    },
+    full () {
+      screenfull.toggle()
+      this.fullIf = !this.fullIf
     }
   }
 }
