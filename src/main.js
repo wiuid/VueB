c3dd652f0f746e5a3a9a480483d75c7e2513acc1
wiuid <li_zhm@qq.com>
2021-04-17 12:07:19 +0800

    适配窄屏的隐藏类

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import store from './store'
 
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+import 'element-ui/lib/theme-chalk/display.css'
 
 import * as echarts from 'echarts'
 
